4ff4e2e6279a6d5a1105c377bdea52e595e49478
Saish Sankhe <saish@saishs-laptop.mynetworksettings.com>
2023-04-08 03:03:02 -0400

    adds toggle to use davinci model and gpt3.5 turbo model

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -5,6 +5,17 @@ const configuration = new Configuration({
 });
 const openai = new OpenAIApi(configuration);
 
+const useDaVinciModel = false;
+
+const systemPrompt = `Assume you are very talented, extremely articulated and an eloquent writer. You specialize in writing day-to-day text messages for humans. Humans will come to you with a word or sentence as a prompt, and  your goal is to craft a message based on the user entered prompt.
+You must not reply to the prompt, but you must generate a message which conveys the same meaning as the prompt.
+You must only and only respond with the generated message. If the message is not in English language, you must not provide its translation in any language.
+You must convert your plain-text response to a JSON object with field - 
+"message": string with the message
+If you cannot generate the message for requested prompt, tone or language, you must respond with an error message.
+You must convert the plain-text error message response to a JSON object with field - 
+"error": string with the error message`;
+
 export default async function (req, res) {
   if (!configuration.apiKey) {
     res.status(500).json({
@@ -16,45 +27,31 @@ export default async function (req, res) {
     return;
   }
 
-  const systemContent = `You are very talented, extremely articulated and an eloquent writer. You specialize in writing day-to-day text messages for humans. Humans will come to you with a word or sentence, and they want you to craft a message based on their requirements.
-  Your goal is to craft a message based on the user entered prompt. You must not reply to the prompt, but you must generate a message which conveys the same meaning as the prompt.
-  You must only and only respond with the generated message. If the message is not in English language, it is fine and you must not provide its translation in any language.
-  You must convert your plain-text response to a JSON object with field - 
-  "message": string with the message
-  If you cannot generate the message for requested prompt, tone or language, you must respond with an error message.
-  You must convert the plain-text error message response to a JSON object with field - 
-  "error": string with the error message`;
+  let body;
 
-  const body = {
-    model: 'gpt-3.5-turbo',
-    messages: [
-      { role: 'system', content: systemContent },
-      { role: 'user', content: generateUserPrompt(req.body) },
-    ],
-    temperature: 0.85,
-  };
+  if (useDaVinciModel) {
+    // TEXT Da-Vinci
+    body = await useDaVinci(systemPrompt, req.body);
+  } else {
+    // CHAT - GPT 3.5 Turbo
+    body = await useGPT35Turbo(systemPrompt, req.body);
+  }
 
   try {
-    const response = await openai.createChatCompletion(body);
-
-    console.log(JSON.parse(response.data.choices[0].message.content));
-
-    try {
+    if (useDaVinciModel) {
+      // TEXT Da-Vinci
+      const response = await openai.createCompletion(body);
+      console.log(`RESPONSE: ${response.data.choices[0].text}`);
+      res.status(200).json({
+        result: JSON.parse(response.data.choices[0].text),
+      });
+    } else {
+      console.log('here');
+      // CHAT - GPT 3.5 Turbo
+      const response = await openai.createChatCompletion(body);
+      console.log(JSON.parse(response.data.choices[0].message.content));
       res.status(200).json({
         result: JSON.parse(response.data.choices[0].message.content),
-      });
-    } catch (error) {
-      console.log(error);
-
-      body.messages.push({
-        role: 'user',
-        content: `Your response wan not valid JSON. The error was ${error.message}. Please respond with just an array of JSON objects and nothing else.`,
-      });
-
-      const retriedResponse = await openai.createChatCompletion(body);
-      console.log(JSON.parse(retriedResponse.data.choices[0].message.content));
-      res.status(200).json({
-        result: JSON.parse(retriedResponse.data.choices[0].message.content),
       });
     }
   } catch (error) {
@@ -72,19 +69,40 @@ export default async function (req, res) {
   }
 }
 
+const useGPT35Turbo = async (systemPrompt, reqBody) => {
+  const body = {
+    model: 'gpt-3.5-turbo',
+    messages: [
+      { role: 'system', content: systemPrompt },
+      { role: 'user', content: generateUserPrompt(reqBody) },
+    ],
+    temperature: 0.85,
+  };
+
+  return body;
+};
+
+const useDaVinci = async (systemPrompt, reqBody) => {
+  const body = {
+    model: 'text-davinci-003',
+    prompt: systemPrompt + generateUserPrompt(reqBody),
+    temperature: 0.85,
+    max_tokens: 1024,
+  };
+
+  return body;
+};
+
 function generateUserPrompt(reqBody) {
   const { prompt, tone, emoticon, language, style, length } = reqBody;
 
-  let createdPrompt = `You must respond with only a JSON object of field "message" or "error".
+  let createdPrompt = `Your response must only be a JSON object of field "message" or "error".
   Using your talent, craft a message for the user, which says - "${prompt}"
-  It should be a ${length} message.
-  The tone of the message should be "${tone}".
-  The language of the output message should be "${language}".
-  The style of the message should be "${style}".`;
+  The message should be of ${length} length, in ${tone} tone, in ${language} language and ${style} style.`;
 
   if (emoticon) {
     createdPrompt = `${createdPrompt}.
-    The message should include emojis.`;
+    The message should include emojis. Include emojis in the string.`;
   }
 
   return createdPrompt;
